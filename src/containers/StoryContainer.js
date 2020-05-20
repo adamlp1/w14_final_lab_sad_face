6212fe9cc2d26cbe0e565bebb58e4d773cc1aed5
adam <adam.lewispetrie@gmail.com>
2020-05-20 14:34:30 +0100

    adds explanation for promise

--- src/containers/StoryContainer.js
+++ src/containers/StoryContainer.js
@@ -10,25 +10,25 @@ class StoryContainer extends React.Component {
 
   componentDidMount(){
 
+    //all of the story ids
     const url = "https://hacker-news.firebaseio.com/v0/topstories.json"
     
-    fetch(url)
-    .then(res => res.json())
+    fetch(url) //asking to get all the story ids
+    .then(res => res.json()) //promising to return json when they've been fetched
+
     .then(topStoriesIds => {
         const allTopStories = []
         topStoriesIds.map( id => {
-            fetch(`https://hacker-news.firebaseio.com/v0/item/${id}.json`)
-            .then(res => res.json())
-            .then(data => allTopStories.push(data))
-            .catch(err => console.err(err))
-            return allTopStories
+            fetch(`https://hacker-news.firebaseio.com/v0/item/${id}.json`) //then we're taking each id and adding it into the story item url
+            .then(res => res.json()) //then we'll turn it into a json object
+            .then(data => allTopStories.push(data)) //then we'll push it into the alltopstories array
+            .catch(err => console.err(err)) 
+            return allTopStories // finally we'll return all of the top stories
         }) 
         console.log(allTopStories)
     })
-    
     .catch(err => console.err(err))
 
-    // then take the story id and add it to the end of the story detail url
     
   }
 
